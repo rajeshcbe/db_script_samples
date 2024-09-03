28dd771df78b481262deda4c689326cd5e0c9789
rajeshcbe <147139450+rajeshcbe@users.noreply.github.com>
2024-09-03 14:01:38 +0530

    Update sql_script.sql

--- sql_script.sql
+++ sql_script.sql
@@ -1,8 +1,9 @@
 CREATE DATABASE  IF NOT EXISTS `rest_db`;
+USE `rest_db`;
 
 DROP TABLE IF EXISTS `report_access_log`;
 CREATE TABLE `report_access_log` (
- `id` varchar(10) NOT NULL,
+ `id` varchar(255) NOT NULL,
  `report_name` varchar (64),
  `parameters` varchar (256),
  `user_name` varchar (64),
